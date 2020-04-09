70bffe9434200914d90c691a54c15daa2e9ebdd8
Dave Hellmer <david.hellmer@ufl.edu>
2020-04-09 16:35:18 -0400

    Daily API allowance table update

--- OSMemberData.py
+++ OSMemberData.py
@@ -54,19 +54,21 @@ for key, value in crData.items():
 
 # Retrieve member profile data for each CRP ID. Each member has their own XML file.
 pfdBasePath = 'D:\\Users\\daveh\\OneDrive\\Documents\\UF\\CIS4914\\Congressional_Finance_Data\\raw_data\\memPFDprofile\\'
-for i in range(25, 150):
+for i in range(150, 300):
     respUrl = pfdReqUrl1 + crList[i] + pfdReqUrl2 + OS_API_KEYS[0]
     resp = requests.get(respUrl)
-    print('resp' + str(i) + ': ' + str(resp.status_code) + '(crp_id: ' + crList[i] + ')')
-    root = etree.fromstring(resp.text)
-    tree = etree.ElementTree(root)
-    filePath = pfdBasePath + crList[i] + '_memPFD.xml'
-    tree.write(filePath, pretty_print=True)
+    if resp.status_code == 200:
+        root = etree.fromstring(resp.text)
+        tree = etree.ElementTree(root)
+        filePath = pfdBasePath + crList[i] + '_memPFD.xml'
+        tree.write(filePath, pretty_print=True)
+    else:
+        print('PfdResp' + str(i) + ': ' + str(resp.status_code) + '(crp_id: ' + crList[i] + ')')
 
 # Retrieve candidate summary data for each CRP ID and cycle. For each year that data exists for a given CRP ID, an XML
 # file is created and saved to a folder named for that cycle.
 candSumBasePath = 'D:\\Users\\daveh\\OneDrive\\Documents\\UF\\CIS4914\\Congressional_Finance_Data\\raw_data\\candSummary\\'
-for i in range(25, 150):
+for i in range(150, 300):
     for j in range(0, 3):
         respUrl = candSumReqUrl1 + crList[i] + candSumReqUrl2 + cycles[j] + candSumReqUrl3 + OS_API_KEYS[j]
         resp = requests.get(respUrl)
@@ -76,12 +78,12 @@ for i in range(25, 150):
             filePath = candSumBasePath + str(cycles[j]) + '\\' + crList[i] + '_candSum.xml'
             tree.write(filePath, pretty_print=True)
         else:
-            print('resp' + str(i) + ': ' + str(resp.status_code) + '(crp_id: ' + crList[i] + ', year: ' + str(cycles[j]) + ')')
+            print('CSumResp' + str(i) + ': ' + str(resp.status_code) + '(crp_id: ' + crList[i] + ', year: ' + str(cycles[j]) + ')')
 
 # Retrieve candidate industry data for each CRP ID and cycle. For each year that data exists for a given CRP ID, an XML
 # file is created and saved to a folder named for that cycle.
 candIndBasePath = 'D:\\Users\\daveh\\OneDrive\\Documents\\UF\\CIS4914\\Congressional_Finance_Data\\raw_data\\candIndustry\\'
-for i in range(25, 150):
+for i in range(150, 300):
     for j in range(0, 3):
         respUrl = candIndReqUrl1 + crList[i] + candIndReqUrl2 + cycles[j] + candIndReqUrl3 + OS_API_KEYS[j]
         resp = requests.get(respUrl)
@@ -91,12 +93,12 @@ for i in range(25, 150):
             filePath = candIndBasePath + str(cycles[j]) + '\\' + crList[i] + '_candInd.xml'
             tree.write(filePath, pretty_print=True)
         else:
-            print('resp' + str(i) + ': ' + str(resp.status_code) + '(crp_id: ' + crList[i] + ', year: ' + str(cycles[j]) + ')')
+            print('CIndResp' + str(i) + ': ' + str(resp.status_code) + '(crp_id: ' + crList[i] + ', year: ' + str(cycles[j]) + ')')
 
 # Retrieve candidate sector data for each CRP ID and cycle. For each year that data exists for a given CRP ID, an XML
 # file is created and saved to a folder named for that cycle.
 candSectBasePath = 'D:\\Users\\daveh\\OneDrive\\Documents\\UF\\CIS4914\\Congressional_Finance_Data\\raw_data\\candSector\\'
-for i in range(25, 150):
+for i in range(150, 300):
     for j in range(0, 3):
         respUrl = candSectReqUrl1 + crList[i] + candSectReqUrl2 + cycles[j] + candSectReqUrl3 + OS_API_KEYS[j]
         resp = requests.get(respUrl)
@@ -106,4 +108,4 @@ for i in range(25, 150):
             filePath = candSectBasePath + str(cycles[j]) + '\\' + crList[i] + '_candSect.xml'
             tree.write(filePath, pretty_print=True)
         else:
-            print('resp' + str(i) + ': ' + str(resp.status_code) + '(crp_id: ' + crList[i] + ', year: ' + str(cycles[j]) + ')')
+            print('CSecResp' + str(i) + ': ' + str(resp.status_code) + '(crp_id: ' + crList[i] + ', year: ' + str(cycles[j]) + ')')
